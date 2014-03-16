4bc12241f99bf83e897c192ef8bc573585fcf77b
Cody Kinsey <internetfett@gmail.com>
2014-03-15 19:25:35 -0500

    bootstrap registration

--- skills/settings.py
+++ skills/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'bootstrap3',
+    'registration',
+    'skills',
 )
 
 MIDDLEWARE_CLASSES = (
